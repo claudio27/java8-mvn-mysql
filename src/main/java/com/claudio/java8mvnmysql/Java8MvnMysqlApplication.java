2e9a482ccf83a2a6addbb70471ef52949ba4b365
Claudio <letresorinterdit@gmail.com>
2018-02-20 00:54:05 -0300

    Added new files from spring initializer

---
+++ src/main/java/com/claudio/java8mvnmysql/Java8MvnMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.claudio.java8mvnmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8MvnMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8MvnMysqlApplication.class, args);
+	}
+}
---
+++ src/test/java/com/claudio/java8mvnmysql/Java8MvnMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.claudio.java8mvnmysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java8MvnMysqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
